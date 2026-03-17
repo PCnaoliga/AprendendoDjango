e841c9dac4f297daec04dd37922187aed1cfbb11
PCnaoliga <rolimpedro15@gmail.com>
2026-03-17 20:33:36 -0300

    Adicionado Complemento no painel adminstrativo

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -14,3 +14,18 @@ class Topic(models.Model):
         # Devolve uma representação em string:
         # # Devolve uma representação em string do modelo
         return self.text
+
+
+class Entry(models.Model):
+    # Algo especifico aprendido sobre um assunto
+    # Passando parametro de uma chave estrangeira
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # Classe para dizer que o plural de entry é entries
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        return self.text[:50]
